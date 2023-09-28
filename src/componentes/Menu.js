4f8995a9547d001807a5488477033009bb265066
Ana Ibacache <131829029+anitabananitaa@users.noreply.github.com>
2023-09-28 21:47:19 +0000

    Agregué imagenes y creamos la barra del menu

--- src/componentes/Menu.js
+++ src/componentes/Menu.js
@@ -1,17 +1,38 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 
-class Menu extends Component {
+export default class Menu extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      desplegar: false,
+    };
+  }
+  desplegar() {
+    this.setState({ desplegar: !this.state.desplegar });
   }
   render() {
     return (
-      <div className="App">
-        <h1> esto es un menú</h1>
+      <div class="botondesplegable">
+        <div class="botonesmenu">
+          <button onClick={() => this.desplegar()} className="toggle-button">
+            ☰
+          </button>
+          <button>
+            <img url="./assets/zonas.png" />
+          </button>
+        </div>
+        {this.state.desplegar && (
+          <div>
+            <ul>
+              <li>Zonas</li>
+              <li>Pacientes</li>
+              <li> Profesionales</li>
+              <li>Llamados</li>
+              <li>Usuarios</li>
+            </ul>
+          </div>
+        )}
       </div>
     );
   }
 }
-
-export default Menu;

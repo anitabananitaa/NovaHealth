e7b094f0c250da73639b7b60a0301194c7651968
Nancy Bonanno <133280964+JustNancyBonanno@users.noreply.github.com>
2023-10-24 01:44:40 +0000

    Update Login.js

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -4,7 +4,7 @@ import Boton from "./Boton";
 import Input from "./Input";
 import ilustracion from "./assets/ilustracion.png";
 
-//const url="http://192.168.0.129:3000/api"//
+//const url="https://192.168.0.129:3000/api"//
 const url="http://localhost:3000/api";
 
 class Login extends Component {

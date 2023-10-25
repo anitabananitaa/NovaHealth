f4c6adda450d1cef92576f64304fc264f806c43c
kiar456 <118364579+kiar456@users.noreply.github.com>
2023-10-25 17:24:08 -0300

    cambiamos el puerto

--- Back/olimpiadas 7-2-1/routes/api/main.js
+++ Back/olimpiadas 7-2-1/routes/api/main.js
@@ -7,7 +7,7 @@ var profesionalesRouter = require('./profesionales/main');
 var usuariosRouter = require('./usuarios/main');
 var zonasRouter = require('./zonas/main');
 
-//http://localhost:3000/api/*
+//http://localhost:3201/api/*
 router.use("/llamados", llamadosRouter)
 router.use("/pacientes", pacientesRouter)
 router.use("/profesionales", profesionalesRouter)
